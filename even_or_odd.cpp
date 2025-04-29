d46f157f437e045c82a126e33c9563a34ff2962b
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-04-29 22:14:15 +0545

    did the odd even whith the use of the ternary operator and some if statements

---
+++ even_or_odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+
+    cout<< "Enter the number: ";
+    cin>>number;
+    if(number == 0 ){
+        cout<< "The entered number is zero so it can't be evern and odd.\n";
+    }else {
+        cout<< "The entered number "<<number<<" is "<<(number%2==0? "Even":"Odd")<<".\n";
+    }
+}
