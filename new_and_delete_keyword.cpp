58e60a455f9ed0753a51d4a1e582afd4e7373e14
Santosh Shrestha <santoshxshrestha@gmail.com>
2025-06-30 16:45:40 +0545

    Added array by the use of the new key word

--- new_and_delete_keyword.cpp
+++ new_and_delete_keyword.cpp
@@ -2,17 +2,33 @@
 using namespace std;
 
 int main() {
-  // Dynamically allocate memory for one integer
-  int *ptr = new int;
+  {
+    // Dynamically allocate memory for one integer
+    int *ptr = new int;
 
-  // Assign value
-  *ptr = 42;
+    // Assign value
+    *ptr = 42;
 
-  // Use the value
-  cout << "Value: " << *ptr << endl;
+    // Use the value
+    cout << "Value: " << *ptr << endl;
 
-  // Free the memory
-  delete ptr;
+    // Free the memory
+    delete ptr;
+  }
+
+  {
+    // Allocate array of 5 integers
+    int *arr = new int[5];
+
+    // Assign and print values
+    for (int i = 0; i < 5; ++i) {
+      arr[i] = i * 10;
+      cout << "arr[" << i << "] = " << arr[i] << endl;
+    }
+
+    // Free the array memory
+    delete[] arr;
+  }
 
   return 0;
 }
