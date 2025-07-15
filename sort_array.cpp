94ed7c80f78f91cbd51b9867da404880045121e1
santoshxshrestha <santoshxshrestha@gmail.com>
2025-07-15 18:32:43 +0545

    added the bubble short method

--- sort_array.cpp
+++ sort_array.cpp
@@ -1,16 +1,17 @@
-#include <iostream>
-int main() {
-  int arr[] = {1, 2, 12, 121, 21, 21};
-  int length = sizeof(arr) / sizeof(arr[0]);
-  for (int i = 0; i < length - 1; i++) {
-    if (arr[i] > arr[i + 1]) {
-      std::swap(arr[i], arr[i + 1]);
-      i = -1;
-    }
-  };
+#include<iostream>
+int main(){
+  int arr[] = {5, 2, 9, 1, 5, 6};
+  int length = sizeof(arr)/sizeof(arr[0]);
 
-  for (int i = 0; i < length; i++) {
-    std::cout << arr[i] << " ";
+  for (int i = 0 ; i<length; i++){
+
+    for (int j = 0; j<length-i-1; j++){
+      if (arr[j] > arr[j+1]){
+        int temp = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = temp;
+      }
+    }
+
   }
-  return 0;
 }
