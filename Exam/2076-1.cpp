47e729dd0f0a8674426415d2c318906db0eb1c08
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-01 23:21:27 +0545

    completed the thigne

--- Exam/2076-1.cpp
+++ Exam/2076-1.cpp
@@ -4,22 +4,54 @@ using namespace std;
 
 class Teacher {
 public:
-  int id;
+  int tid;
   string subject;
 
   void setData(int id, string subject) {
-    this->id = id;
+    this->tid = id;
     this->subject = subject;
   }
 
   void display() {
-    cout << "the id of the teacher is " << id << endl;
+    cout << "the id of the teacher is " << tid << endl;
     cout << "the subject that the teacher teaches is " << subject << endl;
   }
 };
+
+class Staff {
+public:
+  int sid;
+  string position;
+
+  void setData(int id, string position) {
+    this->sid = id;
+    this->position = position;
+  }
+
+  void display() {
+    cout << "the id of the staff is " << sid << endl;
+    cout << "the position that the staff has " << position << endl;
+  }
+};
+
+class Coordinator : public Staff, public Teacher {
+public:
+  string department;
+
+  void setData(string department) { this->department = department; }
+
+  void display() {
+    cout << "the department o fthe coordinator is " << department << endl;
+  }
+}
+
 int main() {
-  Teacher santosh;
-  santosh.setData(44, "rust");
-  santosh.display();
+  Coordinator Santosh;
+  Santosh.setData(Science);
+  Santosh.display();
+
+  Coordinator Anish;
+  Anish.setData(csit);
+  Ansih.display();
   return 0;
 }
