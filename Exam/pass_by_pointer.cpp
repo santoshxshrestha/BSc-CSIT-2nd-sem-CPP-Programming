3a9cecdbb1979368aec86688db8b9f948c31512e
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-03 21:50:54 +0545

    add more thinge and some name chagne

---
+++ Exam/pass_by_pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
+}
+int main(){
+    int a = 43;
+    int b =23;
+    swap(&a, &b);
+    cout << "The value after swap is a:"<< a<< "and b :"<< b<< endl;
+
+    return 0;
+}
--- inline_fn.cpp
+++ inline_fn.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
+/// here we are creating inline func
 inline int square(int x) { return x * x; }
 
 int main() {
+    // instead of calling the fn the inline one it is putted here in the place of the use of it 
   cout << "The square of 4 is " << square(4) << ".\n";
   cout << "The square of 7 is " << square(7) << ".\n";
   return 0;
