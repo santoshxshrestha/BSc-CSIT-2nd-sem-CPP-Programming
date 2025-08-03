ae2faae11fa86e6c24af38c9f3d763efd183c99f
santoshxshrestha <santoshxshrestha@gmail.com>
2025-08-03 21:38:41 +0545

    learned the macro

---
+++ Exam/macro.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define add(a,b)a+b// this is the macro defination
+                   
+int main(){
+    int a = 2;
+    int b = 2;
+    std::cout << "The sum of a and b is "<< add(a,b) << std::endl;
+
+    return 0;
+}
+
