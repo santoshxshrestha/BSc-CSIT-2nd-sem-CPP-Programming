3a76e9de7ef4b2c5610f1ca5e6fb231688975492
santoshxshrestha <santoshxshrestha@gmail.com>
2025-09-05 15:34:09 +0545

    update the tyep

--- Exam/final/functemplate.cpp
+++ Exam/final/functemplate.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-template <typename T> int sum(T a, T b) { return a + b; }
+template <typename T, typename B> void hello(T name, B age) {
+  cout << "hello " << name << ". your age is " << age << endl;
+}
+
+template <typename T> T sum(T a, T b) { return a + b; }
 int main() {
   int a = 43;
   int b = 2;
   cout << "the sum of the two number is " << sum<int>(a, b) << endl;
+
+  string name = "santosh";
+  int age = 21;
+  hello<string, int>(name, age);
   return 0;
 }
